60ab2d02beab1ad0e6d22c31fefda2dc601a4331
Aditya Kumar <adikid1996@gmail.com>
2022-03-03 08:58:04 +0530

    refreshing updates;
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

---
+++ 03-leetcode/525-contigous-array/main.go
@@ -0,0 +1,22 @@
+/***
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+Example 1:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+Example 2:
+
+Input: nums = [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+}
